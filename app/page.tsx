1ddf5e286ee22a68e0ba27eedfcb3b2d4edea761
Milos <milos@webfalcon.com>
2026-03-16 14:30:11 +0100

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentatio
           </a>
         </div>
       </main>
